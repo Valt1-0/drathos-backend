9b387902e099c30be41baa38e342f7d349d75b82
Valt1-0 <lai2val1@gmail.com>
2025-10-31 18:43:12 +0100

    Suppression du commentaire sur le streaming de fichiers pour améliorer la clarté du code

--- src/controllers/serverGameController.js
+++ src/controllers/serverGameController.js
@@ -518,7 +518,6 @@ export const downloadGame = async (req, res) => {
     res.setHeader("X-Accel-Buffering", "no");
     res.setHeader("Cache-Control", "no-cache");
 
-    // Stream le fichier avec des chunks plus gros pour de meilleures performances sur VPN
     const fileStream = fs.createReadStream(safePath, {
       highWaterMark: 2 * 1024 * 1024, // 2 MB chunks
     });
